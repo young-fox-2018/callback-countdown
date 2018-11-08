9bacd16543bc2251703028d50f398abb4856282d
patriagani <patria.gani@gmail.com>
2018-11-08 16:10:53 +0700

    callback countdown done

--- controllers.js
+++ controllers.js
@@ -1,7 +1,18 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let time = seconds
+  let detik = setInterval(function(){
+    if (time > 0) {
+      displayCountdown(time);
+      time = time - 1
+    }
+    else if (time <= 0) {
+      displayCountdown(time)
+      time = time - 1
+      clearInterval(detik)
+    }
+  }, 1000);
 };
 
 module.exports = {
