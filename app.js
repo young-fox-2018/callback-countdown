1e94031e6a159fc07d6aff845fc8a8cc9e3ca1d4
robertarifin <arifinrobert2013@gmail.com>
2018-11-08 15:42:29 +0700

    finish the tasks

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const { timer } = require("./controllers");
 
 const main = () => {
-  const seconds = parseInt(process.argv[2]) || 10;
-  timer(seconds);
+  const seconds = parseInt(process.argv[2]) || 100;
+  timer(seconds)
 };
 
 main();
--- controllers.js
+++ controllers.js
@@ -1,9 +1,27 @@
 const { displayCountdown } = require("./views");
 
-const timer = seconds => {
-  // Your code here...
-};
+const timer = seconds  => {
+  let myVar = setInterval(function() {
+      let minutes = parseInt(seconds / 60)
+      let second = seconds % 60 
+      
+      if (second < 10 && minutes < 10) {
+       displayCountdown(`0${minutes}:0${second}`)
+      } else if (second < 10) {
+        displayCountdown(`${minutes}:0${second}`)
+      } else if (minutes < 10) {
+        displayCountdown(`0${minutes}:${second}`)
+      }
 
-module.exports = {
-  timer
-};
+      if (seconds == 0) {
+        clearInterval(myVar)
+      }
+    
+      seconds -= 1
+    }, 1000)
+  };
+
+  module.exports = {
+    timer, 
+  };
+
