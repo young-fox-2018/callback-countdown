e691f355c71ca3ec95b386c6e856b0a94605dbb0
Kevin Wijaya <Kevin@Macbooknyaa.localdomain>
2018-11-08 18:36:30 +0700

    excercise done

--- controllers.js
+++ controllers.js
@@ -2,6 +2,23 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let interval = setInterval(function(){
+    let menit = String(Math.floor(seconds/60))
+    let detik = String(seconds%60)
+    
+    if(menit.length ==1){
+      menit = "0" + menit
+    }
+    if(detik.length ==1){
+      detik = "0" + detik
+    }
+    displayCountdown(menit, detik)
+    
+    
+    seconds--
+    if(seconds < 0) clearInterval(interval)
+  }, 10);
+  
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -2,10 +2,14 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
-const displayCountdown = seconds => {
+const displayCountdown = (minutes, seconds) => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  // Your code here.
+
+  console.log(chalk.cyan(figlet.textSync(`${minutes} : ${seconds}`)));
+
+
 };
 
 module.exports = { displayCountdown };
