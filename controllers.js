a5b3606df7d6a6f5c68bfd4d6d0eadf7a593f7ee
imanuel vicky <imanuvi@imanuels-MBP.localdomain>
2018-11-08 19:17:10 +0700

    done

--- controllers.js
+++ controllers.js
@@ -1,8 +1,41 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+    let countdown = setInterval(function(){
+      if (seconds > 60) {
+          if (seconds / 60 > 10) {
+            //console.log(`${Math.floor(seconds/60)}: ${seconds % 60}`)
+            if (seconds % 60 <= 10) {
+                displayCountdown(`${Math.floor(seconds/60)}: 0${seconds % 60}`)
+            } else {
+                displayCountdown(`${Math.floor(seconds/60)}: ${seconds % 60}`)
+            }
+          } else {
+            //console.log(`0${Math.floor(seconds/60)}: ${seconds % 60}`)
+            if (seconds % 60 <= 10) {
+                displayCountdown(`0${Math.floor(seconds/60)}: 0${seconds % 60}`)
+            } else {
+              displayCountdown(`0${Math.floor(seconds/60)}: ${seconds % 60}`)
+            }
+          }
+      } else {
+          if (seconds >= 10) {
+             //console.log(`00:${seconds}`)
+             displayCountdown(`00:${seconds}`)
+          } else {
+            //console.log(`00:0${seconds}`)
+            displayCountdown(`00:0${seconds}`)
+          }
+      }
+      if (seconds == 0) {
+        clearInterval(countdown)
+      }
+      seconds -= 1
+    }, 100);
 };
+
+
+
 
 module.exports = {
   timer
