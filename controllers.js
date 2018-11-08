69ff0481cc7ae5e05af73c1e4a18146036bb8e8d
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-11-08 16:22:23 +0700

    done

--- controllers.js
+++ controllers.js
@@ -1,7 +1,37 @@
-const { displayCountdown } = require("./views");
+const displayCountdown  = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  var counter = seconds
+  var set = setInterval(setIn , 1000)
+
+  function setIn(){
+    let menit = Math.floor(counter/60)
+    let menit2 = ''
+    let detik = ''
+
+
+    
+    if(menit < 9) {
+      menit2 = `0${Math.floor(counter/60)}`
+    } else {
+      menit2 = menit
+    }
+    
+    if(detik > 9 ){
+      detik = ` 0${counter - (menit * 60)}`
+    }else {
+      detik = counter - (menit * 60)
+    }
+
+    if(counter >=0){
+      displayCountdown(`${menit2}:${detik}`)
+      counter--
+
+    } else {
+      clearInterval(set)
+    }
+    
+  }
 };
 
 module.exports = {
