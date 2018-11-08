4bacd90b6961db20d9b3efe2db671c9894a2459b
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-11-08 16:26:28 +0700

    Coutdown App

--- controllers.js
+++ controllers.js
@@ -1,7 +1,12 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  for (let i = seconds; i >= 0; i--) {
+    let menit = Math.floor(i / 60)
+    let detik = i % 60
+    if (menit < 9 && detik < 10) displayCountdown(`0${menit} : 0${detik}`)
+    else if (menit < 9 && detik > 10) displayCountdown(`0${menit} : ${detik}`)
+  }
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -2,10 +2,20 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
+}
+
 const displayCountdown = seconds => {
-  clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  console.log(chalk.cyan(figlet.textSync(seconds)));
+  sleep(1000)
+  clear();
 };
 
 module.exports = { displayCountdown };
