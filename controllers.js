0ea06f09c516c718e340d5ee6dd8b7c9f830077a
abednegolubis <lubisabednego@gmail.com>
2018-11-08 19:49:23 +0700

    submit challenge callback belanja

--- controllers.js
+++ controllers.js
@@ -1,7 +1,21 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+    setInterval(function(){ 
+      let menit = Math.floor(seconds/60)
+      let detik = seconds % 60
+      if(menit  < 10 || detik < 10) {
+        menit = `0${menit}`
+        detik = `0${detik}`
+      }
+  
+      displayCountdown(menit,detik); 
+      seconds -= 1
+      if(detik <= 0)  {
+        clearInterval(this);
+      }
+      }, 1000);
+  
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -2,10 +2,14 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
-const displayCountdown = seconds => {
+const displayCountdown = (menit,seconds) => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  console.log(chalk.cyan(figlet.textSync(`${menit} : ${seconds}`)))
+  
+  
+  // console.log(chalk.cyan(figlet.textSync(seconds)))
 };
 
 module.exports = { displayCountdown };
+
