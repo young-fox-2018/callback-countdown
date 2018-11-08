db668cd19b8e0e38eadd632dab5e72acfba4d73e
Arnold Herod <arnold.herod@gmail.com>
2018-11-08 19:42:59 +0700

    initial submission

--- controllers.js
+++ controllers.js
@@ -2,7 +2,31 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  setInterval(function(){
+
+
+    let min = Math.floor(seconds/60)
+    let sec = seconds % 60
+    let display = ``
+    if (min < 10){
+      min = `0${min}`  
+    }
+    
+    if(sec < 10){
+      sec = `0${sec}`
+    }
+
+    display = `${min}:${sec}`
+
+    if(seconds === 0){
+      clearInterval(this)
+    } 
+    displayCountdown(display)
+    seconds--
+    }, 1000)
+
 };
+
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -5,7 +5,8 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  console.log(chalk.red(figlet.textSync(seconds)))
+
 };
 
 module.exports = { displayCountdown };
